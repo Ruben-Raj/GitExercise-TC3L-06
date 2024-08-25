d5069fb3ac3409e8a151d96574f49c5e58c5e3c8
LAKSMEN <laksmen28@gmail.com>
2024-08-26 05:14:23 +0800

    forum

--- app.py
+++ app.py
@@ -3,25 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Configuration for the SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize the database
 db = SQLAlchemy(app)
 
-# Define the Question model
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
 
-# Create the database tables if they don't exist
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def index():
-    questions = Question.query.all()  # Retrieve all questions from the database
+    questions = Question.query.all()  
     return render_template('qna_question.html', questions=questions)
 
 @app.route('/submit-question', methods=['POST'])
