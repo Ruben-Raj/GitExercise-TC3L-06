bd3bdc05a194fa934ffc39e69c244f599452a7de
LAKSMEN <laksmen28@gmail.com>
2024-09-22 04:57:28 +0800

    Remove register files

--- register/__init__.py
+++
@@ -1,8 +0,0 @@
-# register/__init__.py
-
-from flask import Blueprint
-from .routes import *
-
-register_bp = Blueprint('register', __name__, template_folder='templates')
-
-# In the qna/__init__.py file, this blueprint is registered with the URL prefix `/auth`
